5df8ec7af244d7b26d9eb3c3ade31667374ee156
Khaled <khaledrohoma1@gmail.com>
2023-02-24 07:01:12 -0800

    printing square shape.

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -4,19 +4,19 @@
  * print_square - prints a square, followed by a new line;
  * @size: size of the square
  */
-
 void print_square(int size)
 {
 	if (size <= 0)
 	{
 		_putchar('\n');
-	}
-	else
+	}else
 	{
 		int n = size;
-		for (int i = 0; i < n; i++)
+		int i,j;
+
+		for (i = 0; i < n; i++)
 		{
-			for (int j = 0; j < n; j++)
+			for (j = 0; j < n; j++)
 			{
 				_putchar('#');
 			}
