c0d7fd2e9de1243753dce359f091c36bf8557ce7
Khaled <khaledrohoma1@gmail.com>
2023-02-24 08:09:38 -0800

    print numbers and some words intead for a group of numbers.

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,26 +1,33 @@
 #include "main.h"
+#include <stdio.h>
+
 /**
  * main - prints the numbers 1 to 100  
  * print Fizz instead of multiplies of 3
- * print Buzz instead of multiplies of 3 & 5
+ * print Buzz instead of multiplies of 5
  */
 
 int main (void)
 {
 	int i;
-	for (i = 1; i > 100; i++)
+	for (i = 1; i <= 100; i++)
 	{
-		if(i % 3 == 0)
+		if(i % 3 == 0 && i % 5 != 0)
 		{
-			printf("Fizz ");
+			printf(" Fizz");
+		}
+		else if(i % 3 != 0 && i % 5 == 0)
+		{
+			printf(" Buzz");
 		}
 		else if(i % 3 == 0 && i % 5 == 0)
 		{
-			printf("FizzBuzz ");
+			printf(" FizzBuzz");
 		}
 		else
 		{
 			printf("%d ", i);	
 		}
 	}
+	printf("\n");
 }
