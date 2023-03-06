da617a0e8616e051eb87978c1a69247109e2209f
Khaled <khaledrohoma1@gmail.com>
2023-03-06 11:22:48 -0800

    print exactly a certain phrase without puts and printf

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,11 @@
 #include <unistd.h>
 /**
   * main - A program prints a certain sentence without printf of puts functions.
-  * Return - always (1) Success
+  * return - always (1) Success
   */
 int main(void)
 {
-	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
+		, 59);
 	return (1);
 }
